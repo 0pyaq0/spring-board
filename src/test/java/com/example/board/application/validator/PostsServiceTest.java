43eee2bbcb2c562675b59087aebab3dfe25a3164
Benny <devbenny@gsitm.com>
2022-10-27 10:02:07 +0900

    🧪 :: 게시글 테스트

--- src/test/java/com/example/board/application/validator/PostsServiceTest.java
+++ src/test/java/com/example/board/application/validator/PostsServiceTest.java
@@ -1,12 +1,11 @@
-package com.example.board.application.service;
+package com.example.board.application.validator;
 
 import com.example.board.application.dto.PostsDto;
-import com.example.board.domain.Posts;
 import com.example.board.domain.Role;
 import com.example.board.domain.User;
 import com.example.board.infrastructure.persistence.PostsRepository;
 import com.example.board.infrastructure.persistence.UserRepository;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Log4j2
-class PostsServiceTest {
+@Slf4j
+public class PostsServiceTest {
+
     @Autowired
     PostsService postsService;
 
@@ -27,13 +27,17 @@ class PostsServiceTest {
     UserRepository userRepository;
 
     @AfterEach
-    public void clear() {
+    public void clear(){
         postsRepository.deleteAll();
     }
 
     @Test
     public void 게시글_생성() {
-        User user = User.builder().username("benny").nickname("benny").email("benny@benny.com").role(Role.USER).build();
+        User user = User.builder()
+                .username("coco")
+                .nickname("coco")
+                .email("coco@coco.co")
+                .role(Role.USER).build();
 
         PostsDto.Request posts = PostsDto.Request.builder()
                 .title("Test Title")
